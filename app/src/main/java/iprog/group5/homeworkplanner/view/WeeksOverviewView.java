c82770395996ff07b71db1e919b09d8154d69a02
Erica <erica.bronge@gmail.com>
2015-03-04 15:36:45 +0100

    Added an cutom adapter to generate a week list
    Added a custom adapter that will transfor the data from the applicaton's
    weeks array into list items in a listview for the start page. Currently,
    this causes the entire app to crash, but I have not been able to figure
    out why yet.

--- app/src/main/java/iprog/group5/homeworkplanner/view/WeeksOverviewView.java
+++ app/src/main/java/iprog/group5/homeworkplanner/view/WeeksOverviewView.java
@@ -1,7 +1,18 @@
 package iprog.group5.homeworkplanner.view;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
+
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import android.content.Context;
+
 
 import java.util.Observable;
 import java.util.Observer;
@@ -17,19 +28,56 @@ public class WeeksOverviewView implements Observer {
 
     View rootView;
     PlannerModel model;
+    ListView listView;
+    MyArrayAdapter adapter;
 
 
     public WeeksOverviewView(PlannerModel model, View rootView) {
+        listView = (ListView) rootView.findViewById(R.id.week_list);
         this.rootView = rootView;
         this.model = model;
+        this.adapter = new MyArrayAdapter(rootView.getContext(), R.layout.week_list_item, model.getWeeks());
 
-        for (Week week : model.getWeeks()) {
-            //TODO: Add week row for every week, maybe with AdapterView?
-        }
+        listView.setAdapter(adapter);
     }
 
     @Override
     public void update(Observable observable, Object data) {
 
+    }
+
+    //This adapter turns every week into a list item for the week overview view
+    private class MyArrayAdapter extends ArrayAdapter<Week> {
+        Context context;
+        int layoutResourceId;
+        ArrayList<Week> weeks = new ArrayList<Week>();
+
+        private MyArrayAdapter(Context context, int layoutResourceId, ArrayList<Week> weeks) {
+            super(context, layoutResourceId, weeks);
+            this.context = context;
+            this.weeks = weeks;
+            this.layoutResourceId = layoutResourceId;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View item = convertView;
+
+            TextView number;
+            TextView description;
+            Button show;
+
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            item = inflater.inflate(layoutResourceId, parent, false);
+            number = (TextView) item.findViewById(R.id.week_number);
+            description = (TextView) item.findViewById(R.id.week_description);
+            show = (Button) item.findViewById(R.id.show_week);
+
+            Week week = weeks.get(position);
+            number.setText(week.getWeekNumber());
+            description.setText("Test");
+
+            return item;
+        }
     }
 }
