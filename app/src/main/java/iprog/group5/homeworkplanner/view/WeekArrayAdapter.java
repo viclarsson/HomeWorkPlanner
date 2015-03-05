6c5a8206df697efefd3ef5c60fd2c6b06165e5b8
Erica <erica.bronge@gmail.com>
2015-03-05 13:13:04 +0100

    Moved WeekArrayAdapter class to a separate file

---
+++ app/src/main/java/iprog/group5/homeworkplanner/view/WeekArrayAdapter.java
@@ -0,0 +1,66 @@
+package iprog.group5.homeworkplanner.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import iprog.group5.homeworkplanner.R;
+import iprog.group5.homeworkplanner.model.Week;
+
+/**
+ * Created by Erica on 2015-03-05.
+ */
+//This adapter turns every week into a list item for the week overview view
+public class WeekArrayAdapter extends ArrayAdapter<Week> {
+    Context context;
+    int layoutResourceId;
+    ArrayList<Week> weeks = new ArrayList<Week>();
+
+    public WeekArrayAdapter(Context context, int layoutResourceId, ArrayList<Week> weeks) {
+        super(context, layoutResourceId, weeks);
+        this.context = context;
+        this.weeks = weeks;
+        this.layoutResourceId = layoutResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View item = convertView;
+
+        WeekWrapper WeekWrapper = null;
+
+        if (item == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            item = inflater.inflate(layoutResourceId, parent, false);
+            WeekWrapper = new WeekWrapper();
+            WeekWrapper.number = (TextView) item.findViewById(R.id.week_number);
+            WeekWrapper.description = (TextView) item.findViewById(R.id.week_description);
+            WeekWrapper.show = (Button) item.findViewById(R.id.show_week);
+            item.setTag(WeekWrapper);
+        } else {
+            WeekWrapper = (WeekWrapper) item.getTag();
+        }
+
+        Week week = weeks.get(position);
+        WeekWrapper.number.setText(week.getWeekNumber()+"");
+
+        //TODO: This should generate some description of the week, like number of deadlines etc
+        WeekWrapper.description.setText("Test");
+
+        return item;
+    }
+
+    static class WeekWrapper {
+        TextView number;
+        TextView description;
+        Button show;
+    }
+
+}
--- app/src/main/java/iprog/group5/homeworkplanner/view/WeeksOverviewView.java
+++ app/src/main/java/iprog/group5/homeworkplanner/view/WeeksOverviewView.java
@@ -29,14 +29,14 @@ public class WeeksOverviewView implements Observer {
     View rootView;
     PlannerModel model;
     ListView listView;
-    MyArrayAdapter adapter;
+    WeekArrayAdapter adapter;
 
 
     public WeeksOverviewView(PlannerModel model, View rootView) {
-        listView = (ListView) rootView.findViewById(R.id.week_list);
         this.rootView = rootView;
         this.model = model;
-        this.adapter = new MyArrayAdapter(rootView.getContext(), R.layout.week_list_item, model.getWeeks());
+        listView = (ListView) rootView.findViewById(R.id.listview);
+        this.adapter = new WeekArrayAdapter(rootView.getContext(), R.layout.week_list_item, model.getWeeks());
 
         listView.setAdapter(adapter);
     }
@@ -44,40 +44,5 @@ public class WeeksOverviewView implements Observer {
     @Override
     public void update(Observable observable, Object data) {
 
-    }
-
-    //This adapter turns every week into a list item for the week overview view
-    private class MyArrayAdapter extends ArrayAdapter<Week> {
-        Context context;
-        int layoutResourceId;
-        ArrayList<Week> weeks = new ArrayList<Week>();
-
-        private MyArrayAdapter(Context context, int layoutResourceId, ArrayList<Week> weeks) {
-            super(context, layoutResourceId, weeks);
-            this.context = context;
-            this.weeks = weeks;
-            this.layoutResourceId = layoutResourceId;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View item = convertView;
-
-            TextView number;
-            TextView description;
-            Button show;
-
-            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-            item = inflater.inflate(layoutResourceId, parent, false);
-            number = (TextView) item.findViewById(R.id.week_number);
-            description = (TextView) item.findViewById(R.id.week_description);
-            show = (Button) item.findViewById(R.id.show_week);
-
-            Week week = weeks.get(position);
-            number.setText(week.getWeekNumber());
-            description.setText("Test");
-
-            return item;
-        }
     }
 }
