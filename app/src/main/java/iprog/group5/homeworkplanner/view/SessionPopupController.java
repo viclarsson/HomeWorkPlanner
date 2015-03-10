e8508bcb505c5dcdb6bdbb135cf8ef3f5e9bc96a
Erica <erica.bronge@gmail.com>
2015-03-10 16:30:02 +0100

    Added session popup controller
    Added a controller for session popup. It should be able to remove
    sessions, but I can't get it to work.

---
+++ app/src/main/java/iprog/group5/homeworkplanner/view/SessionPopupController.java
@@ -0,0 +1,43 @@
+package iprog.group5.homeworkplanner.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+import iprog.group5.homeworkplanner.ScheduleActivity;
+import iprog.group5.homeworkplanner.model.PlannerModel;
+
+/**
+ * Created by Erica on 2015-03-10.
+ */
+public class SessionPopupController implements View.OnClickListener {
+    public PlannerModel model;
+    public SessionPopupView view;
+    public Activity activity;
+
+    int week_nr;
+    int day_nr;
+    int position;
+
+    public SessionPopupController(PlannerModel model, SessionPopupView view, Activity activity, int week_nr, int day_nr, int position) {
+        this.model = model;
+        this.view = view;
+        this.activity = activity;
+
+        this.week_nr = week_nr;
+        this.day_nr = day_nr;
+        this.position = position;
+
+        // Add listeners here
+        view.removeBtn.setOnClickListener(this);
+        view.doneBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == view.removeBtn) {
+            model.removeSession(week_nr, day_nr, position);
+        }
+    }
+}
