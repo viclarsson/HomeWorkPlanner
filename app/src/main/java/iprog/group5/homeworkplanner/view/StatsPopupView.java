e3a3cb14e0118d74011a7393dbd9dd1cd50e4efe
Erica <erica.bronge@gmail.com>
2015-03-10 13:19:35 +0100

    Added simple bar chart over number of sessions
    Added a very simple bar chart that shows the number of planned sessions
    per subject for the currently viewed week.

--- app/src/main/java/iprog/group5/homeworkplanner/view/StatsPopupView.java
+++ app/src/main/java/iprog/group5/homeworkplanner/view/StatsPopupView.java
@@ -1,13 +1,18 @@
 package iprog.group5.homeworkplanner.view;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
 import iprog.group5.homeworkplanner.R;
 import iprog.group5.homeworkplanner.model.PlannerModel;
+import iprog.group5.homeworkplanner.model.Subject;
 
 /**
  * Created by Erica on 2015-03-09.
@@ -18,16 +23,26 @@ public class StatsPopupView implements Observer {
     PlannerModel model;
     View view;
 
-    TextView weekStatsTest;
-
     public StatsPopupView(PlannerModel model, View view, int week_nr) {
         // Model and root view
         this.model = model;
         this.view = view;
 
-        weekStatsTest = (TextView) view.findViewById(R.id.week_stats_test);
+        LinearLayout chart =  (LinearLayout) view.findViewById(R.id.chart);
+        HashMap<Subject, Integer> sessionCounts = this.model.getWeeks().get(week_nr).getSessionSubjectsCount();
 
-        weekStatsTest.setText("Här kan vi ha lite statistik för veckan.");
+        for (HashMap.Entry<Subject, Integer> entry : sessionCounts.entrySet()) {
+            Subject subject = entry.getKey();
+            int value = entry.getValue();
+
+            TextView newView = new TextView(view.getContext());
+            newView.setWidth(50);
+            newView.setHeight(value * 50);
+            newView.setBackgroundColor(subject.getColor());
+            newView.setText(value + "");
+
+            chart.addView(newView);
+        }
     }
 
     @Override
