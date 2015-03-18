4f9c62288c3538e92e5543fcc69116e2224a3a23
Erica <erica.bronge@gmail.com>
2015-03-18 11:27:22 +0100

    Made the stats bar chart view correct information

--- app/src/main/java/iprog/group5/homeworkplanner/view/StatsPopupView.java
+++ app/src/main/java/iprog/group5/homeworkplanner/view/StatsPopupView.java
@@ -1,6 +1,7 @@
 package iprog.group5.homeworkplanner.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
 import iprog.group5.homeworkplanner.R;
+import iprog.group5.homeworkplanner.model.Assignment;
 import iprog.group5.homeworkplanner.model.PlannerModel;
 import iprog.group5.homeworkplanner.model.Subject;
 
@@ -25,26 +28,61 @@ public class StatsPopupView implements Observer {
     PlannerModel model;
     View view;
 
+    int barWidth = 60;
+
     public StatsPopupView(PlannerModel model, View view, int week_nr) {
         // Model and root view
         this.model = model;
         this.view = view;
 
         LinearLayout chart =  (LinearLayout) view.findViewById(R.id.chart);
+        LinearLayout chartLabels = (LinearLayout) view.findViewById(R.id.chart_labels);
         HashMap<Subject, Integer> sessionCounts = model.getWeek(week_nr).getSessionSubjectsCount();
 
         for (HashMap.Entry<Subject, Integer> entry : sessionCounts.entrySet()) {
             Subject subject = entry.getKey();
             int value = entry.getValue();
 
-            TextView newView = new TextView(view.getContext());
-            newView.setWidth(70);
-            newView.setHeight(value * 50);
-            newView.setBackgroundColor(subject.getColor());
-            newView.setText(value + "");
-            newView.setGravity(Gravity.BOTTOM);
+            //Don't count custom sessions to the stats
+            if (!subject.getName().equalsIgnoreCase("custom")) {
 
-            chart.addView(newView);
+                int color = subject.getColor();
+                float[] hsv = new float[3];
+                Color.colorToHSV(color, hsv);
+                hsv[2] *= 0.6f; // value component
+                int darker_color = Color.HSVToColor(hsv);
+
+                //Create a bar for the subject
+                TextView newBarView1 = new TextView(view.getContext());
+                newBarView1.setWidth(barWidth);
+                newBarView1.setHeight(value * barWidth);
+                newBarView1.setBackgroundColor(color);
+                newBarView1.setText(value + "");
+                newBarView1.setGravity(Gravity.BOTTOM);
+
+                Assignment assignment = model.getWeek(week_nr).getAssignmentBySubject(subject);
+
+                //Create a bar for the estimated workload for the assignment in this subject
+                TextView newBarView2 = new TextView(view.getContext());
+                newBarView2.setWidth(barWidth);
+                newBarView2.setHeight((assignment.getEstimatedWorkLoad() / 30) * barWidth);
+                newBarView2.setBackgroundColor(darker_color);
+                newBarView2.setText((assignment.getEstimatedWorkLoad() / 30) + "");
+                newBarView2.setGravity(Gravity.BOTTOM);
+
+                chart.addView(newBarView1);
+                chart.addView(newBarView2);
+
+                //Add subject labels below the bars
+                TextView newLabelView = new TextView(view.getContext());
+                newLabelView.setWidth(barWidth * 2);
+                newLabelView.setText(subject.getName());
+                newLabelView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                chartLabels.addView(newLabelView);
+            }
+
+
         }
     }
 
